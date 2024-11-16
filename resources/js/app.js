ba12e1665273cc764668398ae8215e6971d7d8cc
jisungyoo <jisung87kr@gmail.com>
2024-11-17 00:54:09 +0900

    pinia 패키지 설치

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,11 @@
 import './bootstrap';
 import { createApp } from "vue";
 import components from "./components";
+import { createPinia } from "pinia";
 
 const app = createApp({});
-// const pinia = createPinia();
-// app.use(pinia);
+const pinia = createPinia();
+app.use(pinia);
 
 Object.entries(components).forEach(([name, component]) => {
     app.component(name, component);
